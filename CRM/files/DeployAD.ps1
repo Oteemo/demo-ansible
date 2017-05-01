34527c728d4267403e06ae086435d6b3697d4312
Greg Pryzby <gpryzby@gmail.com>
2017-05-01 15:01:19 -0400

    deploy AD

---
+++ CRM/files/DeployAD.ps1
@@ -0,0 +1,20 @@
+#
+# Windows PowerShell script for AD DS Deployment
+#
+
+# Install the module
+Install-windowsfeature AD-domain-services
+
+# Install/config AD
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "Win2012R2" `
+-DomainName "TS.local" `
+-DomainNetbiosName "TS" `
+-ForestMode "Win2012R2" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
