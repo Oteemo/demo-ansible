91f793a859be596ff21d200a68ac938f74b1bd98
Greg Pryzby <gpryzby@gmail.com>
2017-05-02 18:34:09 -0400

    path typo fix

--- CRM/files/Deploy-CRM.ps1
+++ CRM/files/Deploy-CRM.ps1
@@ -5,11 +5,11 @@
 # Download the CRM binary
 $url = "https://download.microsoft.com/download/6/A/B/6AB1209C-8091-4BFA-B7B5-D4FFA454CA48/CRM2013-Server-ENU-amd64.exe"
 $output = "C:\Windows\CRM2013-Server-ENU-amd64.exe"
-
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
 
-cd c:\Windows\Temp
-CRM2013-Server-ENU-amd64.exe extract:./crm2012
-cd crm
-C:\Windows\Temp\crm\SetupServer.exe /config c:\Windows\Temp\crm2012.xml
+# Extract the install
+c:\Windows\Temp\CRM2013-Server-ENU-amd64.exe /extract:c:\Windows\Temp\crm2013
+
+# Run installer with config file
+C:\Windows\Temp\crm2013\SetupServer.exe /config c:\Windows\Temp\crm2012.xml
