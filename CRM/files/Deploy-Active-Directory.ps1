7aa06a072baf64edd9d17e81194b0c896b0e4707
Greg Pryzby <gpryzby@gmail.com>
2017-05-02 13:49:34 -0400

    AD user, ou and group setting

--- CRM/files/Deploy-Active-Directory.ps1
+++ CRM/files/Deploy-Active-Directory.ps1
@@ -20,4 +20,18 @@ Install-ADDSForest `
 -Force:$true `
 -SafeModeAdministratorPassword (convertto-securestring "SpaceGhost!!1" -asplaintext -force)
 
+# Add domain controller and promote
 Install-ADDSDomainController -InstallDns -DomainName "TS.local" -Force:$true
+
+# Add user
+New-ADUser -Name "CRM Admin" -GivenName "CRM" -Surname "Admin" `
+-SamAccountName "crm.admin" -UserPrincipalName "crm.admin@TS.local" `
+-AccountPassword (convertto-securestring "SpaceGhost!!1" -asplaintext -force) `
+-PasswordNeverExpires:$true `
+-PassThru | Enable-ADAccount
+
+# Add to Performance Log User Group
+Add-ADGroupMember -Identity "Performance Log Users" -Member "crm.admin"
+
+# Add OU
+NEW-ADOrganizationalUnit "CRM"
