efd76542ec2a6c2c256ff5f46928db167a832681
Greg Pryzby <gpryzby@gmail.com>
2017-05-02 20:35:53 -0400

    typo

--- CRM/files/Install-CRM.bat
+++ CRM/files/Install-CRM.bat
@@ -1,9 +1,2 @@
-#
-# Windows script for CRM 
-#
-
-# Extract the install
 C:\Windows\Temp\CRM2013-Server-ENU-amd64.exe /extract:c:\Windows\Temp\crm2013
-
-# Run installer with config file
 C:\Windows\Temp\crm2013\SetupServer.exe  /config C:\Windows\Temp\crm2012.xml
