5efa06184e3809b5262ef013926abcce4b0c941a
Greg Pryzby <gpryzby@gmail.com>
2017-05-03 11:58:43 -0400

    log the installation of CRM

--- CRM/files/Install-CRM.bat
+++ CRM/files/Install-CRM.bat
@@ -1,2 +1,2 @@
 C:\Windows\Temp\CRM2013-Server-ENU-amd64.exe /extract:c:\Windows\Temp\crm2013
-C:\Windows\Temp\crm2013\SetupServer.exe  /config C:\Windows\Temp\crm2012.xml
+C:\Windows\Temp\crm2013\SetupServer.exe /Q /L C:\Windows\Temp\CRM2013-install.log /config C:\Windows\Temp\crm2012.xml 
