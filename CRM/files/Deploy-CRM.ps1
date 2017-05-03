a76651374d5ed85e4b20dfe9f8940cd7d16aa4c3
Greg Pryzby <gpryzby@gmail.com>
2017-05-02 20:12:17 -0400

    ps1 learning

--- CRM/files/Deploy-CRM.ps1
+++ CRM/files/Deploy-CRM.ps1
@@ -4,7 +4,7 @@
 
 # Download the CRM binary
 $url = "https://download.microsoft.com/download/6/A/B/6AB1209C-8091-4BFA-B7B5-D4FFA454CA48/CRM2013-Server-ENU-amd64.exe"
-$output = "C:\Windows\CRM2013-Server-ENU-amd64.exe"
+$output = "C:\Windows\Temp\CRM2013-Server-ENU-amd64.exe"
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
 
