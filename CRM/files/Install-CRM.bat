893f0583710f9d7e6ebbc1e25c99c82e04f2d9a7
Greg Pryzby <gpryzby@gmail.com>
2017-05-03 21:24:09 -0400

    update trial key

--- CRM/files/Install-CRM.bat
+++ CRM/files/Install-CRM.bat
@@ -1,2 +1,2 @@
-C:\Windows\Temp\CRM2013-Server-ENU-amd64.exe /extract:c:\Windows\Temp\crm2013
-C:\Windows\Temp\crm2013\SetupServer.exe /Q /L C:\Windows\Temp\CRM2013-install.log /config C:\Windows\Temp\crm2012.xml 
+\Windows\Temp\CRM2013-Server-ENU-amd64.exe /extract:\Windows\Temp\crm2013
+\Windows\Temp\crm2013\SetupServer.exe /Q /L \Windows\Temp\CRM2013-install.log /config C:\Windows\Temp\crm2012.xml 
