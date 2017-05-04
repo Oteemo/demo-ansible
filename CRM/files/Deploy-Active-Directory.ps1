bbee529cdbf1c96033013a4b8448cc82eb31482b
Greg Pryzby <gpryzby@gmail.com>
2017-05-03 23:12:15 -0400

    typos on domain

--- CRM/files/Deploy-Active-Directory.ps1
+++ CRM/files/Deploy-Active-Directory.ps1
@@ -10,12 +10,12 @@ Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath "C:\Windows\NTDS" `
 -DomainMode "Win2012R2" `
--DomainName "TSRM.local" `
+-DomainName "TSCRM.local" `
 -DomainNetbiosName "TSCRM" `
 -ForestMode "Win2012R2" `
 -InstallDns:$true `
 -LogPath "C:\Windows\NTDS" `
-#-NoRebootOnCompletion:$false `
+-NoRebootOnCompletion:$false `
 -SysvolPath "C:\Windows\SYSVOL" `
 -Force:$true `
 -SafeModeAdministratorPassword (convertto-securestring "SpaceGhost!!1" -asplaintext -force)
