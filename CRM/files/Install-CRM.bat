a8bacdb3797816d2fc3e4310bf59f2a849298f85
Greg Pryzby <gpryzby@gmail.com>
2017-05-05 11:45:36 -0400

    reboot after starting OLAP

--- CRM/files/Install-CRM.bat
+++ CRM/files/Install-CRM.bat
@@ -1,2 +1,1 @@
-\Windows\Temp\CRM2013-Server-ENU-amd64.exe /extract:\Windows\Temp\crm2013
 \Windows\Temp\crm2013\SetupServer.exe /Q /L \Windows\Temp\CRM2013-install.log /config C:\Windows\Temp\crm2012.xml 
