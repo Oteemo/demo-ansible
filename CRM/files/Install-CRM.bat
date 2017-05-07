58e889ce66b229320d1fc8a297f1a8329ce28d5f
Greg Pryzby <gpryzby@gmail.com>
2017-05-07 15:37:55 -0400

    restart olap

--- CRM/files/Install-CRM.bat
+++ CRM/files/Install-CRM.bat
@@ -1,1 +1,1 @@
-\Windows\Temp\CRM-2013\SetupServer.exe /Q /L \Windows\Temp\CRM2013-install.log /config C:\Windows\Temp\crm2013.xml 
+\Windows\Temp\CRM-2013\SetupServer.exe /QR /L \Windows\Temp\CRM2013-install.log /config C:\Windows\Temp\crm2013.xml 
